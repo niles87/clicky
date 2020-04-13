8f11d8ecbf4cea5ea38919fd5df4c3ea4abc0c38
niles87 <nbing19@gmail.com>
2020-04-12 20:46:02 -0700

    removed h3 element from nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,10 +6,9 @@ function Navbar(props) {
     <div>
       <nav className="navbar" id="navbar">
         <h5 className="navbar-brand ">Clicky Game</h5>
-        <h3>Click an image to start!</h3>
         <div>
           <h5 className="user-score">
-            Current Score: {props.score} ~ Best Score: {props.bestScore}
+            Current Score: {props.score} - Best Score: {props.bestScore}
           </h5>
         </div>
       </nav>
