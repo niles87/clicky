94db54e7c6ffffae25c28335c9fe78a7c22447eb
niles87 <nbing19@gmail.com>
2020-04-12 20:41:42 -0700

    added a on click prop to the card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,12 @@ import "./style.css";
 
 function Card(props) {
   return (
-    <div className="card" data-clicked={props.clicked} data-id={props.id}>
+    <div
+      className="card"
+      onClick={() => props.onClick(props.id, props.clicked)}
+      data-clicked={props.clicked}
+      data-id={props.id}
+    >
       <div className="img-container">
         <img src={props.image} className="card-img" alt={props.name} />
       </div>
