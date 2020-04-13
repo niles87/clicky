784e5bae6147dfa7452f54cb38a326314ee8d1c5
niles87 <nbing19@gmail.com>
2020-04-12 20:40:45 -0700

    added functionality to handle click function, added a shuffle function to rearrange the cards

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -7,12 +7,27 @@ import "./style.css";
 
 class Body extends Component {
   state = {
-    scooby,
+    scooby: scooby,
     score: 0,
     bestScore: 0,
   };
 
-  handleClick = () => {};
+  shuffle = (arr) => {
+    arr.sort(() => Math.random() - 0.5);
+    return arr;
+  };
+
+  handleClick = (id, chosen) => {
+    let clicked = this.state.scooby.map((character) => {
+      if (character.id === id) {
+        character.clicked = true;
+      }
+      return character;
+    });
+    console.table(clicked);
+    let scoobyChars = this.shuffle(clicked);
+    this.setState({ scooby: scoobyChars });
+  };
 
   render() {
     return (
@@ -22,7 +37,7 @@ class Body extends Component {
         <div className="card-container">
           {this.state.scooby.map((character) => (
             <Card
-              handleClick={this.handleClick}
+              onClick={this.handleClick}
               id={character.id}
               key={character.id}
               name={character.name}
