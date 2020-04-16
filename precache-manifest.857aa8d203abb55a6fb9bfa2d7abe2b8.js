867e58e1e56d8ef4b31f9f1c772cb769738e0896
niles87 <nbing19@gmail.com>
2020-04-16 13:38:56 -0700

    Updates

---
+++ precache-manifest.857aa8d203abb55a6fb9bfa2d7abe2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc4d162a15c7a41eec621424dd6b4ac",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "fcb9ae8910de7efa2dc1",
+    "url": "/clicky/static/css/main.1b03bdec.chunk.css"
+  },
+  {
+    "revision": "6317c9b25b6891613dd1",
+    "url": "/clicky/static/js/2.242e5220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky/static/js/2.242e5220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb9ae8910de7efa2dc1",
+    "url": "/clicky/static/js/main.666671c3.chunk.js"
+  },
+  {
+    "revision": "1ab451366b9ad674527a",
+    "url": "/clicky/static/js/runtime-main.b40413aa.js"
+  }
+]);
